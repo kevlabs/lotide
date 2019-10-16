a3b1ce095f80aa4c6389808e29a8f340a3a58aa7
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-16 12:38:14 -0400

    eqObj fn

---
+++ eqObjects.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  let output = '';
+  if (actual === expected) {
+    output = `\x1b[32m✅Assertion Passed: ${actual} === ${expected}\x1b[0m`;
+  } else {
+    output = `\x1b[31m❗️️️Assertion Failed: ${actual} !== ${expected}\x1b[0m`;
+  }
+  console.log(output);
+  
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  if (keys1.length !== Object.keys(object2).length) return false;
+  for (const key of keys1) {
+    if (typeof object1[key] !== 'object') {
+      //compare primitives first
+      if (object1[key] !== object2[key]) return false;
+    } else {
+      //elseif object use recurrence and call eqObjects again
+      if (!eqObjects(object1[key], object2[key])) return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
