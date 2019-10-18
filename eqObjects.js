e2cc542a5d88c7a35f132fe7fa482ae053f766e2
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-18 15:11:24 -0400

    Adeded test cases

--- eqObjects.js
+++ eqObjects.js
@@ -40,3 +40,9 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { z: 1, f: {e: 3, c: 2} }, b: 2 }, { a: { z: 1, f: {e: 3, c: 2} }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
