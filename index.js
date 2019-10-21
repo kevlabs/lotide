201f07aca38a7d208fd5fd7d4f6fd2134b51f088
Kevin Laburte <kevinlaburte@Kevins-MacBook-Pro-2.local>
2019-10-21 13:09:21 -0400

    Added master file index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqObjects = require('./eqObjects');
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
+  eqObjects,
+  assertObjectsEqual
+};
